5cd4e5bbbfc34cfc15f1d0fa12ff1f364189f10f
Alexandr <net_box@ukr.net>
2021-09-17 06:43:12 +0300

    feat: add @media

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,25 @@ window.addEventListener('DOMContentLoaded', function (){
         video.insertAdjacentHTML('afterbegin', '<iframe src="' + src +'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
     });
 })
+
+
+
+$(function(){
+
+    $('.header-nav__mob').on('click', function(){
+        $('.header-nav__mob').toggleClass('header-nav__mob--active');
+        $('.header-nav__items').toggleClass('header-nav__items--active');
+    });
+
+    $('.header-nav__item-link').on('click', function(){
+        $('.header-nav__mob').removeClass('header-nav__mob--active');
+        $('.header-nav__items').removeClass('header-nav__items--active');
+    });
+
+    $(".header-nav__item-link").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 2000);
+	});
+});
